6c539eb196d2118b686eb02fcdc1c722d9377008
Miguel Jaramillo <m.jrillo@live.com>
2023-09-07 10:22:11 -0600

    add alt attribute to img

--- src/computerplayer.js
+++ src/computerplayer.js
@@ -11,7 +11,7 @@ export default function ComputerPlayer(props){
     return(
         <div className="player-card">
             <p>Player {props.index}:</p>
-            <img className="card" src={unknownCard}/>
+            <img className="card" src={unknownCard} alt=''/>
             {/*<h3>{props.card.rank} of {props.card.suit}</h3>*/}
         </div>
     );
--- src/humanplayer.js
+++ src/humanplayer.js
@@ -2,7 +2,7 @@ export default function HumanPlayer(props){
     return(
         <div className="player-card">
             <p>Player {props.index}:</p>
-            <img className="card" src={props.card.url}/>
+            <img className="card" src={props.card.url} alt=''/>
             {/*<h3>{props.card.rank} of {props.card.suit}</h3>*/}
             <div className="player-controls">
                 <button onClick={() => props.handleDraw(props.index)}>Draw</button>
